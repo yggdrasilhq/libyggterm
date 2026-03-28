b42b86310e22d28b78c2c20c785811a40616ebfb
Avikalpa Kundu <avi@gour.top>
2026-03-28 15:04:56 +0530

    Add app control channel and native Linux app capture

--- crates/yggui/src/lib.rs
+++ crates/yggui/src/lib.rs
@@ -2,6 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+mod app_capture;
 pub mod chrome;
 pub mod drag_tree;
 pub mod drag_visuals;
