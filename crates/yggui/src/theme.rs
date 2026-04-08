4bdcef5396bb2a46af521543a302a641fc50e0c8
Avikalpa Kundu <avi@gour.top>
2026-04-08 15:42:29 +0530

    Split yggui contract from yggterm shell

--- crates/yggui/src/theme.rs
+++ crates/yggui/src/theme.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use yggterm_core::{UiTheme, YgguiThemeColorStop, YgguiThemeSpec};
+use yggui_contract::{UiTheme, YgguiThemeColorStop, YgguiThemeSpec};
 
 pub const MAX_THEME_STOPS: usize = 6;
 pub const THEME_EDITOR_SWATCHES: [&str; 8] = [
